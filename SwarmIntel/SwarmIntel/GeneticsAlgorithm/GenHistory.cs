82dca144ec85325ba9ef1cea404db2d3c7591e5b
cloudman88 <adamanan88@gmail.com>
2017-05-28 16:28:43 +0300

    add genetics engine, tools and problems

---
+++ SwarmIntel/SwarmIntel/GeneticsAlgorithm/GenHistory.cs
@@ -0,0 +1,15 @@
+﻿namespace ChilisExp.GeneticsAlgorithms
+{
+    class GenHistory<T> where T : Gen
+    {
+        public T BestGen { get; set; }
+        public double Avg { get; set; }
+        public double StdDev { get; set; }
+        public GenHistory(T bestGen, double avg, double stdDev)
+        {
+            BestGen = bestGen;
+            Avg = avg;
+            StdDev = stdDev;
+        }
+    }
+}
---
+++ SwarmIntel/SwarmIntel/Tools/CVR_Probelm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwarmIntel.Tools
+{    
+    enum CvrProblemsFiles
+    {
+        fdfd,
+    }
+    // ReSharper disable once InconsistentNaming
+    public class CVR_Probelm
+    {
+
+    }
+}
